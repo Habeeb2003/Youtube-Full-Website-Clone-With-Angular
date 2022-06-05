d01da97fb54086d86bef2d35d6562bd23ebe6ff8
Habeeb2003 <tawaliuwilliams@gmail.com>
2022-06-05 17:35:33 +0100

    feat(watch page ui done)

--- src/app/comment-section/comment-section.component.ts
+++ src/app/comment-section/comment-section.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentSectionComponent implements OnInit {
 
+  comments = [
+    {},{},{},{},{},{},{}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatchComponent implements OnInit {
 
+  relatedVideos = [
+    {},{},{},{},{},{},{},{},{},{}
+  ] 
+
   constructor() { }
 
   ngOnInit(): void {
