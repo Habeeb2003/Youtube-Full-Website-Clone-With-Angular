f417466ad4b34633be4c7bc1acae0e685a85b221
Habeeb2003 <tawaliuwilliams@gmail.com>
2022-06-21 12:25:31 +0100

    feat(result page): "filter by upload date working"

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -26,6 +26,8 @@ export class ResultsComponent implements OnInit {
     {filter: 'videoType', value: 'movie'}
   ]
 
+  activeTimeFilter : 'hour' | 'today' | 'week' | 'month' | 'year' | undefined = undefined
+
   videoFilter: filterType = { filter : 'type', value: 'video'}
 
   constructor(private resultService: SearchResultService) { }
@@ -54,8 +56,8 @@ export class ResultsComponent implements OnInit {
 
   filterSearch(filter: filterType[]){
     console.log(filter);
-    
     this.resultService.changeFilters(filter)
+    this.open = false
   }
 
   checkIfActive(e: MouseEvent) : boolean{
@@ -64,6 +66,52 @@ export class ResultsComponent implements OnInit {
     // }
     console.log((e.target as HTMLLinkElement).classList.contains('active'));
     return (e.target as HTMLLinkElement).classList.contains('active')
+  }
+
+  getDateForFilter(param : 'Y' | 'M' | 'W' | 'H' | 'T' | undefined) : string{
+    let timeFilter
+    let date
+    if (param == undefined) {
+      this.filterSearch([{filter: "publishedAfter", value: undefined}])
+    }
+    switch (param) {
+      case 'Y':
+        timeFilter = 1000 * 60 * 60 * 24 * 365
+        date = new Date(new Date().valueOf() - timeFilter)
+        this.filterSearch([{filter: "publishedAfter", value: date.toISOString()}])
+        this.activeTimeFilter = "year"
+        break;
+      case 'M':
+        timeFilter = 1000 * 60 * 60 * 24 * 30
+        date = new Date(new Date().valueOf() - timeFilter)
+        this.filterSearch([{filter: "publishedAfter", value: date.toISOString()}])
+        this.activeTimeFilter = "month"
+        break;
+      case 'W':
+        timeFilter = 1000 * 60 * 60 * 24 * 7
+        date = new Date(new Date().valueOf() - timeFilter)
+        this.filterSearch([{filter: "publishedAfter", value: date.toISOString()}])
+        this.activeTimeFilter = "week"
+        break;
+      case 'H':
+        timeFilter = 1000 * 60 * 60
+        date = new Date(new Date().valueOf() - timeFilter)
+        this.filterSearch([{filter: "publishedAfter", value: date.toISOString()}])
+        this.activeTimeFilter = "hour"
+        break;
+      case 'T':
+        timeFilter = 1000 * 60 * 60 * 24
+        date = new Date(new Date().valueOf() - timeFilter)
+        this.filterSearch([{filter: "publishedAfter", value: date.toISOString()}])
+        this.activeTimeFilter = "today"
+        break;
+      case undefined:
+        this.activeTimeFilter = undefined
+        break;
+      default:
+        break;
+    }
+    return "";
   }
 
   // changeFilters<K extends keyof searchResultInterface>(parameter: { filter: K, value: searchResultInterface[K] }){
